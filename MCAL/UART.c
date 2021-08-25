84a56a82eb6498dfbfd2f3b6a1779c31b051a615
ADELELDEEB <89468490+ADELELDEEB@users.noreply.github.com>
2021-08-25 13:53:59 +0200

    Update UART.c

--- MCAL/UART.c
+++ MCAL/UART.c
@@ -21,23 +21,6 @@ void UART_ini_RX()
          GPIO_PORTB_AMSEL_R = 0x00;
 }
 
-void UART_ini_TX()
-{
-    SYSCTL_RCGCUART_R |= 0x02;
-         SYSCTL_RCGCGPIO_R |= 0x02;
-
-         UART1_CTL_R &= ~0x01;
-         UART1_IBRD_R = 104;
-         UART1_FBRD_R = 11;
-         UART1_LCRH_R |= 0x07;
-         UART1_CTL_R |= 0x301;
-
-         GPIO_PORTB_AFSEL_R |= 0x20;
-         GPIO_PORTB_DEN_R |= 0x20;
-         GPIO_PORTB_PCTL_R |= 0x10;
-         GPIO_PORTB_AMSEL_R = 0x00;
-}
-
 void RX()
 {
     while((UART1_FR_R & (1<<4)) != 0);
@@ -50,22 +33,4 @@ void RX()
           else if ((UART1_DR_R & 0xFF) == 0x00)
               GPIO_PORTF_DATA_R &= ~0x0E;
 }
-
-void TX()
-{
-    while(1){
-           unsigned long SW1,y;
-           SW1 = GPIO_PORTF_DATA_R & 0x01;
-            if(SW1 == 0x01){
-                y = 0x00;
-           }
-           if(SW1 == 0x00){
-               y = 0x01;
-           }
-           while((UART1_FR_R & (1<<5)) != 0);
-           UART1_DR_R = y;
-
-       }
-}
-
 
