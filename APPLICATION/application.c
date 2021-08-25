211dc91e46e4b6d715a03bd7a9b4e5eaabfea407
ADELELDEEB <89468490+ADELELDEEB@users.noreply.github.com>
2021-08-25 13:48:52 +0200

    Update application.c

--- APPLICATION/application.c
+++ APPLICATION/application.c
@@ -11,25 +11,15 @@
 
 void apply()
 {
-
-     char RED='W';
      UART_ini_RX();
-   //  UART_ini_TX();
      SW_ini();
      LedInit();
      INITIALIZE_SPI_MASTER();
-
 while(1)
 {
     RX();
-   if(!(GPIO_PORTF_DATA_R & 0x10))
-    {
-       SPI_Transmit(RED);
-    }
-
+    SPI_Transmit(ASCII());
 }
-
-
 }
 
 
